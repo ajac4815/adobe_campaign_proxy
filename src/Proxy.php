5bb5ecd0fddec9d516cfb2bd4d1f35183fd548a7
Adam Jacobs <adam.jacobs@nih.gov>
2024-10-22 10:38:35 -0400

    Add request error handling

--- src/Proxy.php
+++ src/Proxy.php
@@ -4,6 +4,7 @@ namespace Drupal\adobe_campaign_proxy;
 
 use Drupal\Core\Cache\CacheBackendInterface;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
 
 /**
  * Proxy class to connect to Adobe Campaign API.
@@ -57,27 +58,32 @@ class Proxy {
       if (!$_SERVER['ADOBE_API_KEY'] || !$_SERVER['ADOBE_CLIENT_SECRET']) {
         return FALSE;
       }
-      $token_request = $this->client->request(
-      'POST',
-      $this::GENERATE_TOKEN_URL,
-      [
-        'form_params' => [
-          'grant_type' => 'client_credentials',
-          'client_id' => $_SERVER['ADOBE_API_KEY'],
-          'client_secret' => $_SERVER['ADOBE_CLIENT_SECRET'],
-          'scope' => 'openid,AdobeID,campaign_sdk,additional_info.projectedProductContext,campaign_config_server_general,deliverability_service_general',
-        ],
-      ]
-      );
-      $token_body = $token_request->getBody();
-      if ($token_body) {
-        $token_json = json_decode($token_body);
-        if ($token_json) {
-          $token = $token_json->access_token;
-          $token_expire = $time + $token_json->expires_in;
-          $this->cache->set('adobe_campaign_token', base64_encode($token), $token_expire);
-          return $token;
+      try {
+        $token_request = $this->client->request(
+        'POST',
+        $this::GENERATE_TOKEN_URL,
+        [
+          'form_params' => [
+            'grant_type' => 'client_credentials',
+            'client_id' => $_SERVER['ADOBE_API_KEY'],
+            'client_secret' => $_SERVER['ADOBE_CLIENT_SECRET'],
+            'scope' => 'openid,AdobeID,campaign_sdk,additional_info.projectedProductContext,campaign_config_server_general,deliverability_service_general',
+          ],
+        ]
+        );
+        $token_body = $token_request->getBody();
+        if ($token_body) {
+          $token_json = json_decode($token_body);
+          if ($token_json) {
+            $token = $token_json->access_token;
+            $token_expire = $time + $token_json->expires_in;
+            $this->cache->set('adobe_campaign_token', base64_encode($token), $token_expire);
+            return $token;
+          }
         }
+      }
+      catch (GuzzleException $e) {
+        return FALSE;
       }
       return FALSE;
     }
@@ -86,25 +92,40 @@ class Proxy {
 
   /**
    * Send API get request.
+   *
+   * @return array|false
+   *   Array of json response data or false.
    */
   public function get() {
     $token = $this->generateToken();
     if ($token) {
       $org = $_SERVER['ADOBE_ORG'];
       if ($org) {
-        $request = $this->client->request(
-        'GET',
-        "https://mc.adobe.io/{$org}/campaign/profileAndServices/profile",
-        [
-          'headers' => [
-            'Authorization' => "Bearer {$token}",
-            'Cache-Control' => 'no-cache',
-            'Content-Type' => 'application/json',
-            'X-Api-Key' => $_SERVER['ADOBE_API_KEY'],
-          ],
-        ]
-        );
-        return $request->getBody();
+        try {
+          $request = $this->client->request(
+          'GET',
+          "https://mc.adobe.io/{$org}/campaign/profileAndServices/profile",
+          [
+            'headers' => [
+              'Authorization' => "Bearer {$token}",
+              'Cache-Control' => 'no-cache',
+              'Content-Type' => 'application/json',
+              'X-Api-Key' => $_SERVER['ADOBE_API_KEY'],
+            ],
+          ]
+          );
+          $body = $request->getBody();
+          if ($body) {
+            $json = json_decode($body);
+            if ($json) {
+              return $json;
+            }
+          }
+          return FALSE;
+        }
+        catch (GuzzleException $e) {
+          return FALSE;
+        }
       }
     }
   }
@@ -117,21 +138,26 @@ class Proxy {
     if ($token) {
       $org = $_SERVER['ADOBE_ORG'];
       if ($org) {
-        $request = $this->client->request(
-        'POST',
-        'https://mc.adobe.io/ninds-mkt-stage1/campaign/profileAndServices/profile',
-        [
-          'headers' => [
-            'Authorization' => "Bearer {$token}",
-            'Cache-Control' => 'no-cache',
-            'Content-Type' => 'application/json',
-            'X-Api-Key' => $_SERVER['ADOBE_API_KEY'],
-          ],
-          'json' => [
-            "email" => "adam.jacobs@nih.gov",
-          ],
-        ]
-        );
+        try {
+          $request = $this->client->request(
+          'POST',
+          'https://mc.adobe.io/ninds-mkt-stage1/campaign/profileAndServices/profile',
+          [
+            'headers' => [
+              'Authorization' => "Bearer {$token}",
+              'Cache-Control' => 'no-cache',
+              'Content-Type' => 'application/json',
+              'X-Api-Key' => $_SERVER['ADOBE_API_KEY'],
+            ],
+            'json' => [
+              "email" => "adam.jacobs@nih.gov",
+            ],
+          ]
+          );
+        }
+        catch (GuzzleException $e) {
+          return FALSE;
+        }
       }
     }
   }
