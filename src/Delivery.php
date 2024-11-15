7e1634d8604c5e6fe42260805fc095e7c3c326c4
Jacobs <jacobsaw@ns02332192md.mynetworksettings.com>
2024-11-15 16:07:43 -0500

    Add workflow trigger and template for send methods

--- src/Delivery.php
+++ src/Delivery.php
@@ -36,4 +36,37 @@ class Delivery {
     $this->proxy->post($endpoint, $data);
   }
 
+  /**
+   * Send trigger signal to progress workflow.
+   *
+   * @param string $workflow_id
+   *   The ID of the workflow.
+   * @param array $parameters
+   *   The data to pass to the workflow.
+   */
+  public function sendWorkflowSignal(string $workflow_id, array $parameters) {
+    // Required for external signal activity.
+    $data['source'] = 'API';
+    $data['parameters'] = $parameters;
+    // @todo Get endpoint here.
+    $endpoint = '';
+    $this->proxy->post($endpoint, $data);
+  }
+
+  /**
+   * Complete workflow-based email delivery.
+   *
+   * @param string $workflow_id
+   *   The ID of the workflow that will be used.
+   * @param array $parameters
+   *   Data to pass to the workflow.
+   */
+  public function send(string $workflow_id, array $parameters) {
+    // @todo make both methods private.
+    $this->startWorkflow($workflow_id);
+    // @todo Replace with real sleep.
+    sleep(5);
+    $this->sendWorkflowSignal($workflow_id, $parameters);
+  }
+
 }
