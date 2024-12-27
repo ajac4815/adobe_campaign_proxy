c36bea68c4920c35f845783093050799f41f645d
Adam Jacobs <adam.jacobs@nih.gov>
2024-12-27 15:46:19 -0500

    Add state based workflow error handling

--- src/Delivery.php
+++ src/Delivery.php
@@ -2,10 +2,19 @@
 
 namespace Drupal\adobe_campaign_proxy;
 
+use Psr\Log\LoggerInterface;
+
 /**
  * Email send integration.
  */
 class Delivery {
+
+  /**
+   * The proxy logger channel.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
 
   /**
    * The proxy API service.
@@ -19,9 +28,27 @@ class Delivery {
    *
    * @param Proxy $proxy
    *   The proxy API service.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The proxy logger channel.
    */
-  public function __construct(Proxy $proxy) {
+  public function __construct(Proxy $proxy, LoggerInterface $logger) {
+    $this->logger = $logger;
     $this->proxy = $proxy;
+  }
+
+  /**
+   * Get workflow information.
+   *
+   * @param string $workflow_id
+   *   The ID of the workflow.
+   *
+   * @return string
+   *   The workflow state (if available).
+   */
+  private function getWorkflow(string $workflow_id) {
+    $endpoint = "workflow/execution/{$workflow_id}";
+    $response = $this->proxy->get($endpoint);
+    return $response->state ?? '';
   }
 
   /**
@@ -29,11 +56,33 @@ class Delivery {
    *
    * @param string $workflow_id
    *   The ID of the workflow to start.
+   *
+   * @return bool
+   *   Success indicator of start attempt.
    */
   private function startWorkflow(string $workflow_id) {
     $endpoint = "workflow/execution/{$workflow_id}/commands";
     $data = ['method' => 'start'];
     $this->proxy->post($endpoint, $data);
+    // Confirm start.
+    $workflowResponse = $this->getWorkflow($workflow_id);
+    if (!$workflowResponse) {
+      return FALSE;
+    }
+    $start = time();
+    $timeout = 60;
+    while ($workflowResponse !== 'started') {
+      // Timeout and reattempt later if more than a minute passes.
+      if (time() > $start + $timeout) {
+        return FALSE;
+      }
+      $workflowResponse = $this->getWorkflow($workflow_id);
+      // Stop at any request failures.
+      if (!$workflowResponse) {
+        return FALSE;
+      }
+    }
+    return TRUE;
   }
 
   /**
@@ -45,6 +94,9 @@ class Delivery {
    *   The ID of the signal activity within the workflow.
    * @param array $parameters
    *   The data to pass to the workflow.
+   *
+   * @return bool
+   *   Indicates whether send succeeded or failed.
    */
   private function sendWorkflowSignal(string $workflow_id, string $signal_id, array $parameters) {
     // Prepare data for signal.
@@ -59,7 +111,7 @@ class Delivery {
         '',
         $trigger_url
       );
-      $this->proxy->post($endpoint, $data);
+      return $this->proxy->post($endpoint, $data) ? TRUE : FALSE;
     }
   }
 
@@ -72,13 +124,24 @@ class Delivery {
    *   Data to pass to the workflow.
    */
   public function send(string $workflow_id, array $parameters) {
-    // @todo make both methods private.
-    // @todo Handle bad response throughout.
-    $this->startWorkflow($workflow_id);
-    // @todo Replace with real sleep.
-    sleep(10);
-    // @todo Pass this signal ID via some sort of configuration...
-    $this->sendWorkflowSignal($workflow_id, 'signal1', $parameters);
+    // Confirm workflow is in expected state (stopped).
+    $initialState = $this->getWorkflow($workflow_id);
+    if (!$initialState) {
+      return FALSE;
+    }
+    elseif ($initialState !== 'stopped') {
+      $this->logger->warning("Workflow {$workflow_id} not stopped when attempting new send.");
+      return FALSE;
+    }
+    // Attempt to start the workflow.
+    // Must be in "started" state prior to triggering external signal.
+    $success = $this->startWorkflow($workflow_id);
+    if ($success) {
+      // @todo Pass this signal ID via some sort of configuration...
+      // @todo Check for confirmation, at least from send.
+      return $this->sendWorkflowSignal($workflow_id, 'signal1', $parameters);
+    }
+    return FALSE;
   }
 
 }
